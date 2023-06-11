b107cc8cb7885c1ca236255a7e771ab00c941875
Giridhar Nair <giridhar.r.nair@gmail.com>
2023-06-11 23:29:58 +0530

    Multiple machines can connect

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { useState, useRef } from 'react'
-import reactLogo from './assets/react.svg'
+import { useRef } from 'react'
 import Editor from "@monaco-editor/react"
 import * as Y from "yjs"
 import { WebrtcProvider } from "y-webrtc"
@@ -11,27 +10,27 @@ import { MonacoBinding } from "y-monaco"
 function App() {
   const editorRef = useRef(null);
 
-  // Editor value -> YJS Text value (A text value shared by multiple people)
-  // One person deletes text -> Deletes from the overall shared text value
-  // Handled by YJS
-
-  // Initialize YJS, tell it to listen to our Monaco instance for changes.
-
   function handleEditorDidMount(editor, monaco) {
     editorRef.current = editor;
-    // Initialize YJS
-    const doc = new Y.Doc(); // a collection of shared objects -> Text
-    // Connect to peers (or start connection) with WebRTC
-    const provider = new WebrtcProvider("test-room", doc); // room1, room2
-    const type = doc.getText("monaco"); // doc { "monaco": "what our IDE is showing" }
-    // Bind YJS to Monaco 
+    const ydoc = new Y.Doc(); 
+    const yarray = ydoc.getArray()
+    const provider = new WebrtcProvider('webrtc-test', ydoc, { signaling: ['ws://localhost:4444']})
+    const type = ydoc.getText("monaco"); 
     const binding = new MonacoBinding(type, editorRef.current.getModel(), new Set([editorRef.current]), provider.awareness);
-    console.log(provider.awareness);                
+    console.log(provider.awareness);     
+    
+    provider.on('synced', synced => {
+      console.log('synced!', synced)
+    })
+
+    yarray.observeDeep(() => {
+      console.log('yarray updated: ', yarray.toJSON())
+    })
   }
 
   return (
     <Editor
-      height="100vh"
+      height="50vh"
       width="100vw"
       theme="vs-dark"
       onMount={handleEditorDidMount}
