f4cb20d3a952a1fef1a4f7192438bf4e932ec8bf
GiridharRNair <49298134+GiridharRNair@users.noreply.github.com>
2025-02-26 12:52:17 -0600

    Delete 1260-day-of-the-year directory

--- 1260-day-of-the-year/day-of-the-year.py
+++
@@ -1,6 +0,0 @@
-class Solution(object):
-    def dayOfYear(self, date):
-        from datetime import datetime
-
-        dt = datetime.strptime(date, "%Y-%m-%d")
-        return dt.timetuple().tm_yday
